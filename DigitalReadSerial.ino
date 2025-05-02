dbe934a7f1f1cbeb2b3a63904ca1a9962f99f53d
董庭婕 <s111319020@stu.ntue.edu.tw>
2025-05-02 12:01:33 +0800

    Add files via upload

---
+++ DigitalReadSerial.ino
@@ -0,0 +1,56 @@
+byte seven_seg_digits[10][7] = { 
+{ 1,1,1,1,1,1,0 }, // = 0
+{ 0,1,1,0,0,0,0 }, // = 1
+{ 1,1,0,1,1,0,1 }, // = 2
+{ 1,1,1,1,0,0,1 }, // = 3
+{ 0,1,1,0,0,1,1 }, // = 4
+{ 1,0,1,1,0,1,1 }, // = 5
+{ 1,0,1,1,1,1,1 }, // = 6
+{ 1,1,1,0,0,0,0 }, // = 7
+{ 1,1,1,1,1,1,1 }, // = 8
+{ 1,1,1,0,0,1,1 } // = 9
+};
+
+byte pin_number[7]={2,3,4,5,6,7,8};
+const int buttonPin = 12;
+int buttonState = 0;
+int num = 0;
+
+void setup() {
+  for(byte pin;pin<7;pin++){
+    pinMode( pin,OUTPUT);
+  }
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+// pinMode(9, OUTPUT);
+pinMode(buttonPin,INPUT_PULLUP);
+Serial.begin(9600);
+}
+
+void sevenSegWrite(byte digit) {
+byte pin = 2;
+for (byte seg = 0; seg < 7; seg++) {
+digitalWrite(pin,seven_seg_digits[digit][seg]);
+pin++;
+}
+}
+
+void loop() {
+
+ 
+ buttonState = digitalRead(buttonPin);
+ sevenSegWrite(num);
+ delay(1000);
+ if(buttonState == 0){
+ num ++;
+ if(num > 9){
+ num = 0;
+ }
+}
+
+}
